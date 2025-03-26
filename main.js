d114d46db1bd0bf1d0e2f1bfcb96f94ec3cd0b24
rizwan <rizwanrrsweb@gmail.com>
2025-03-26 12:41:19 +0530

    added  new name

--- main.js
+++ main.js
@@ -92,3 +92,8 @@ next.addEventListener('click', function(){
   let items = document.querySelectorAll('.item')
   document.querySelector('.slide').appendChild(items[0])
 })
+
+prev.addEventListener('click', function(){
+  let items = document.querySelectorAll('.item')
+  document.querySelector('.slide').prepend(items[items.length - 1])
+})
